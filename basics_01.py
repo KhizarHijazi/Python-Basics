caebab1dfa03c9c10b644a5a4a50980d21a6bdd5
KhizarHijazi <khizarraza126@gmail.com>
2024-10-17 13:11:52 +0500

    Initial commit

---
+++ basics_01.py
@@ -0,0 +1,7 @@
+print('Khizar Hijazi')
+
+def functionName(k):
+    print(k)
+
+# call the function
+functionName("here's the function input value")
---
+++ imported.py
@@ -0,0 +1,3 @@
+from basics_01 import functionName
+
+functionName("I have imported a function from the first file and given it a new value here. The imported function assigns the new value rather than the basic function.")
