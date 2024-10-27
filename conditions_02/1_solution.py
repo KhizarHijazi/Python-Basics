c4f515e1724490f9d3fdd9a73b92f991ab601810
KhizarHijazi <khizarraza126@gmail.com>
2024-10-27 14:14:00 +0500

    added problem solving folder named conditions_02

---
+++ conditions_02/1_solution.py
@@ -0,0 +1,10 @@
+age = input("Enter your age :")
+age_in_int = int(age)
+
+if age_in_int < 12 :
+    print("you are child")
+elif age_in_int < 18 :
+    print("you are teenager")
+elif age_in_int >= 18 :
+    print("you are adult")
+    
